10c94bc63a9cea604497a07e8c2b9a79f16666b2
Sergey Siplivy <s.siplivyj@gmail.com>
2018-04-18 22:44:16 +0300

    Add restore NuGet packages task for build procedure

--- build.fsx
+++ build.fsx
@@ -4,11 +4,21 @@ open Fake
 
 // Properties
 let buildDir = "./build/"
+let solutionFile = "PackageChecker/PackageChecker.sln"
+let solutionPackagesPath = "PackageChecker/packages"
 
 // Targets
 Target "Clean" (fun _ ->
   CleanDir buildDir
 )
+
+Target "RestorePackages" (fun _ -> 
+  solutionFile
+    |> RestoreMSSolutionPackages (fun p ->
+      { p with
+          OutputPath = solutionPackagesPath
+          Retries = 4 })
+ )
 
 Target "BuildApp" (fun _ ->
   !! "PackageChecker/**/*.csproj"
@@ -22,6 +32,7 @@ Target "Default" (fun _ ->
 
 // Dependencies
 "Clean"
+  ==> "RestorePackages"
   ==> "BuildApp"
   ==> "Default"
 
