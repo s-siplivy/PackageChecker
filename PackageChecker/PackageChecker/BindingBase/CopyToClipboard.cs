2c47eac0d71219ca70cb393ac047b895d2283eac
Sergey Siplivy <s.siplivyj@gmail.com>
2018-05-01 15:13:08 +0300

    Issue #11: Add the ability to copy text from grid
    Context menu with Copy value command is added

---
+++ PackageChecker/PackageChecker/BindingBase/CopyToClipboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PackageChecker.BindingBase
+{
+	public class CopyToClipboard : ICommand
+	{
+		public string Name { get { return "Copy"; } }
+
+		public void Execute(object parameter)
+		{
+			Clipboard.SetText(parameter != null ? parameter.ToString() : "<null>");
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public event EventHandler CanExecuteChanged;
+	}
+}
